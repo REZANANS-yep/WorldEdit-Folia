aacc119a35fb975a58014504a8cd1d58eb5eb840
TomyLobo <tomylobo@nurfuerspam.de>
2026-04-19 22:46:34 +0200

    Add BlockState.getMaterial()

--- worldedit-core/src/main/java/com/sk89q/worldedit/world/block/BlockType.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/world/block/BlockType.java
@@ -72,7 +72,7 @@ public class BlockType implements Keyed {
     @SuppressWarnings("this-escape")
     private final LazyReference<BlockMaterial> blockMaterial
         = LazyReference.from(() -> WorldEdit.getInstance().getPlatformManager()
-        .queryCapability(Capability.GAME_HOOKS).getRegistries().getBlockRegistry().getMaterial(this));
+        .queryCapability(Capability.GAME_HOOKS).getRegistries().getBlockRegistry().getMaterial(getDefaultState()));
     @SuppressWarnings("this-escape")
     @Deprecated
     private final LazyReference<String> name = LazyReference.from(() -> WorldEdit.getInstance().getPlatformManager()
